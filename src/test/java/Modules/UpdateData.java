375dc20ceb9e620d324f9f319b260ce324e8787f
Givan Cosmin <40201128+GivanCosmin@users.noreply.github.com>
2019-09-17 12:59:09 +0300

    UpdateData script corrected

--- src/test/java/Modules/UpdateData.java
+++ src/test/java/Modules/UpdateData.java
@@ -24,20 +24,20 @@ public class UpdateData {
         Response response = httpRequest.request(Method.GET);
         JsonPath jsonPathEvaluator = response.jsonPath();
         List<String> id = jsonPathEvaluator.get("id");
-        System.out.println(id.get(0));
+        System.out.println(id.get(10));
 
         RestAssured.baseURI ="http://dummy.restapiexample.com/api/v1";
         RequestSpecification request = RestAssured.given();
 
         //Create jsonobject with attributes and send it to the database + assert to verify output
         JSONObject requestParams = new JSONObject();
-        requestParams.put("id",id.get(5));
+        requestParams.put("id",id.get(10));
         requestParams.put("name", "dummyName"); // Cast
         requestParams.put("salary", "dummySalary");
         requestParams.put("age", "dummyAge");
 
         request.body(requestParams.toJSONString());
-        Response response2 = request.put("/update/"+id.get(5));
+        Response response2 = request.put("/update/"+id.get(10));
         System.out.println("Response body: " + response2.body().asString());
         Assert.assertEquals(response2.body().asString(),"{\"name\":\"dummyName\",\"id\":\""+id.get(10)+"\",\"salary\":\"dummySalary\",\"age\":\"dummyAge\"}");
 
